1ac14e99f44080008da34f67b7f8aa65d865003e
funnyscar <43627632+curtisjhu@users.noreply.github.com>
2025-05-29 03:04:32 -0700

    Initial commit

---
+++ shaders/frag.glsl
@@ -0,0 +1,9 @@
+uniform vec2 u_resolution;
+uniform float u_time;
+
+void main()
+{
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+    vec3 col = 0.5 + 0.5*cos(u_time+uv.xyx+vec3(0,2,4));
+    gl_FragColor = vec4(col,1);
+} 
